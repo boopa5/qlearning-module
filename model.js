d9ce9d6f1ea7136a77ec7e86cc7a6b50915995a0
boopa5 <rgao.2003@gmail.com>
2021-12-28 17:54:49 -0800

    fix model

--- model.js
+++ model.js
@@ -1,24 +1,21 @@
-class Model {
+export default class Model {
     constructor({
         learningRate = 0.1,
         epsilon = 0.1,
         discountFactor = 0.9,
         epsilonDecay = 0.99,
-        rewardFunc = (state) => 0,
         qTable = {}
     } = {
         learningRate: 0.5,
         epsilon: 0.1,
         discountFactor: 0.9,
         epsilonDecay: 0.9,
-        rewardFunc: (state) => 0,
         qTable: {}
     }) {
         this._learningRate = learningRate;
         this._epsilon = epsilon;
         this._discountFactor = discountFactor;
         this._epsilonDecay = epsilonDecay;
-        this._rewardFunc = rewardFunc;
         this._qTable = qTable;
     }
 
@@ -66,39 +63,51 @@ class Model {
         this._qTable = qTable;
     }
 
-    updateQValue(state, action, newState, newActions, reward) {
-        let oldValue = this.getQValue(state, action);
-        let y = this._rewardFunc(state) + this._gamma * this.getQValue(newState, this.argmax(a => this.getQValue(newState, a), newActions));
+    decayEpsilon() {
+        this._epsilon *= this._epsilonDecay;
+    }
 
-        this._qTable[state][action] = oldValue + this._learningRate * (y - oldValue);
+    updateQValue(state, action, newState, newActions, reward) {
+        let oldValue = this.getQValue(state, action, "old");
+        console.log("newState: " + JSON.stringify(newState));
+        console.log("newActions: " + JSON.stringify(newActions));
+        let y = reward + this._discountFactor * this.getQValue(newState, this.argmax(a => this.getQValue(newState, a), newActions));
+
+        this._qTable[JSON.stringify(state)][action] = oldValue + this._learningRate * (y - oldValue);
     }
 
     argmax(func, inputs) {
-        let index = 0;
-        for (let i = 0; i < inputs.length; i++) {
-            if (func(inputs[i]) > func(inputs[index])) {
-                index = i;
+        let index = [0];
+        for (let i = 1; i < inputs.length; i++) {
+            if (func(inputs[i]) > func(inputs[index[0]])) {
+                index = [i];
+            } else if (func(inputs[i]) == func(inputs[index[0]])) {
+                index.push(i);
             }
         }
-        return inputs[index];
+        return inputs[index[Math.floor(Math.random() * index.length)]];
     }
 
     getAction(state, actions) {
         let action;
-        if (Math.random < this._epsilon) {
+        if (Math.random() < this._epsilon) {
             action = actions[Math.floor(Math.random() * actions.length)];
         }
         action = this.argmax(a => this.getQValue(state, a), actions);
-        this._epsilon *= this._epsilonDecay;
+
         return action;
     }
 
     getQValue(state, action) {
+        state = JSON.stringify(state);
+
+        if (this._qTable[state] === undefined) {
+            this._qTable[state] = {};
+            this._qTable[state][action] = 0;
+        }
         if (this._qTable[state][action] === undefined) {
             this._qTable[state][action] = 0;
         }
         return this._qTable[state][action];
     }
 }
-
-export default Model;
